23ef52eba33813b2ffe70ed301154e135407c0da
LohainyOliveira <102706685+LohainyOliveira@users.noreply.github.com>
2022-03-30 22:49:14 -0300

    Add files via upload

---
+++ ProjetoDeCalculadora/Calculadora.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetoDeCalculadora
+{
+	/// <summary>
+	/// Lógica interna para Calculadora.xaml
+	/// </summary>
+	public partial class Calculadora : Window
+	{
+		public Calculadora()
+		{
+			InitializeComponent();
+		}
+
+		private void btSomar_Click(object sender, RoutedEventArgs e)
+		{
+			double numeroUm = Convert.ToDouble(textNumeroUm.Text);
+			double numeroDois = Convert.ToDouble(textNumeroDois.Text);
+
+			double resultado = numeroUm + numeroDois;
+			textResultado.Text = Convert.ToString(resultado);
+		}
+
+		private void btSubtrair_Click(object sender, RoutedEventArgs e)
+		{
+			double numeroUm = Convert.ToDouble(textNumeroUm.Text);
+			double numeroDois = Convert.ToDouble(textNumeroDois.Text);
+
+			double resultado = numeroUm - numeroDois;
+			textResultado.Text = Convert.ToString(resultado);
+		}
+
+		private void btDividir_Click(object sender, RoutedEventArgs e)
+		{
+			double numeroUm = Convert.ToDouble(textNumeroUm.Text);
+			double numeroDois = Convert.ToDouble(textNumeroDois.Text);
+
+			double resultado = numeroUm / numeroDois;
+			textResultado.Text = Convert.ToString(resultado);
+		}
+
+		private void btMultiplicar_Click(object sender, RoutedEventArgs e)
+		{
+			double numeroUm = Convert.ToDouble(textNumeroUm.Text);
+			double numeroDois = Convert.ToDouble(textNumeroDois.Text);
+
+			double resultado = numeroUm * numeroDois;
+			textResultado.Text = Convert.ToString(resultado);
+		}
+	}
+}
---
+++ ProjetoDeCalculadora/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetoDeCalculadora
+{
+	/// <summary>
+	/// Interação lógica para MainWindow.xam
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+        private void btAcessar_Click(object sender, RoutedEventArgs e)
+        {
+
+            string senha = textSenha.Password.ToString();
+            string user = textUsuario.Text;
+
+            if (user == "Lohainy" & senha == "1234")
+            {
+                MessageBox.Show("Logado!");
+                textUsuario.Clear();
+                textSenha.Clear();
+                Calculadora calculadora = new Calculadora();
+                calculadora.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorreto!");
+                textUsuario.Clear();
+                textSenha.Clear();
+            }
+        }
+	}
+}
